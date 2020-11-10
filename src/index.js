6adbf6c638c13254d2431034710ffa3246d4dba8
triggxl <triggxl@hotmail.com>
2020-11-10 16:32:20 -0500

    set up test env. variable, working on deploying with Vercel

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-// console.log(process.env);
+// console.log(process.env); (would normally remove)
 ReactDOM.render(
   <BrowserRouter>
     <App />
