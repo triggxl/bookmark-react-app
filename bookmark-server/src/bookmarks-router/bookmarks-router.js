88f0d07177f16d891c1488edbfd3e2210c169d77
triggxl <triggxl@hotmail.com>
2021-01-27 18:51:34 -0500

    made changes in bookmarks-router to resubmit assignment

--- bookmark-server/src/bookmarks-router/bookmarks-router.js
+++ bookmark-server/src/bookmarks-router/bookmarks-router.js
@@ -12,43 +12,53 @@ const bodyParser = express.json();
 bookmarksRouter
   .route('/bookmarks')
   .get((req, res) => res.send(bookmarks))
+    /*
+[N] Write a route handler for POST /bookmarks that accepts a JSON object representing a bookmark and adds it to the list of bookmarks after validation.
+I always receive a bookmark with undefined not received error regardless of whether I put in valid data or invalid data. For invalid data, I should see an error message about what field is invalid. For valid data, a created bookmark should be returned.
+  */
   .post(bodyParser, (req, res) => {
     //accepting data params from request
     const { id, title, url, rating } = req.body;
     console.log(req.body)
     if(!id) {
-      //req.params = url params
-      logger.error(`Bookmark with ${id} not recieved.`);
+      logger.error(`Invalid, id: ${id}.`);
       //ajax
-      res.status(404).send(`Bookmark ${id} not recieved.`);
+      res.status(404).send(`Invalid, id: ${id}.`);
     }
     else if(!title) {
-      logger.error(`Bookmark with ${title} not found.`);
-      res.status(404).send({error: `${title} not recieved.`});
+      logger.error(`Invalid, title: ${title}.`);
+      res.status(404).send({error: `Invalid, title: ${title}.`});
     }
     else if(!url) { 
-      logger.error(`Bookmark with ${url} not found.`); 
-      res.status(404).send({error: `${url} not recieved.`});
+      logger.error(`Invalid, url: ${url}.`); 
+      res.status(404).send({error: `url: ${url} not recieved.`});
     }
     else if(!rating) {
-      logger.error(`Bookmark with ${rating} not found.`); 
-      res.status(404).send({error: `${rating} not recieved.`});
+      logger.error(`Invalid, rating: ${rating}.`); 
+      res.status(404).send({error: `Invalid, rating: ${rating}.`});
     }
-    else if(req.body) {
+    else {
       bookmarks.push(req.body)
-      // res.status(200).send({'Added new bookmark successfully.'})
+      // res.status(200).send({'Added new bookmark successfully.'}) only need to send once
       res.json(req.body)
     }
   })
+  /*
+  I receive a 200 OK status and a blank response regardless of whether I look for a bookmark that exists or doesn't exist. 
+  If it doesn't exist, I should receive a 404. 
+  If it does, I should receive the bookmark record as JSON.
+  Solution: Pick valid/invalid id (use postman)
+  */
   bookmarksRouter
   .route('/bookmarks/:id')
   .get((req, res) => {
     //find()
     const { id } = req.params;
     const bmMatch = bookmarks.find(bm => bm.id === id)
-    if(!bookmarks) {
+    if(!bmMatch) {
       logger.Error(`Bookmark with ${id} not found.`);
       res.status(404).send('Bookmark not found.');
+      return
     }
     res.json(bmMatch)
   })
